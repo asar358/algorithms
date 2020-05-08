e72fb4f77e9ec9402adcbdf099712e32ba272aa2
Felix Asare <asarefelix440@gmail.com>
2020-05-08 01:10:45 -0500

    fibonachi sequence

---
+++ wk1day4.js
@@ -0,0 +1,16 @@
+ function fibonachi(n){
+    let output = [];
+    if (n === 1){
+        output = [0];
+    } else if (n === 2){
+     output = [0, 1];
+    } else {
+        output = [0, 1];
+        for (let i = 2; i < n; i++){
+         output.push(output[output.length -2] + output[output.length -1]);
+        }
+}
+return output
+}
+output = fibonachi(5)
+console.log(output)
